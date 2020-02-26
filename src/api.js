df0bc6a9daabbba13214977a5b70fac1071d1a27
PaolaS2992 <huaripayta29.92@gmail.com>
2020-02-26 00:12:45 -0500

    Prueba de versiones

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const mdLinks = require('./mdlinks.js');
 
-const ruta = '/home/paolasonia/Desktop/5_LABORATORIA/En_mi_disco_local_C/00-Laboratoria/04-LIM011-fe-md-links/LIM011-fe-md-links/src/README.md';
+const ruta1 = '/home/paolasonia/Desktop/5_LABORATORIA/En_mi_disco_local_C/00-Laboratoria/04-LIM011-fe-md-links/LIM011-fe-md-links/src/README.md';
 
-mdLinks(ruta, { validate: true }).then((r) => console.log(r));
+mdLinks(ruta1, { validate: true }).then((r) => console.log(r));
